d024fea5f2324a689039034c838b61d4838015b6
AdamSWS <ashaar2@uic.edu>
2024-06-14 22:16:56 -0500

    removed scroll

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,31 +24,29 @@ export default function Main() {
 
   return (
     <motion.main
-      className="flex flex-col items-center w-full text-white py-16 overflow-hidden"
+      className="flex items-center w-full text-white py-16 overflow-hidden"
       initial={{ x: '100%', opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       transition={{ duration: 1 }}
     >
-      <h2 className="text-4xl font-bold mb-2">Welcome to Our Projects</h2>
-      <div className="w-full flex justify-center">
-        <hr className="border-t-2 border-gray-600 w-3/4 mb-8" />
-      </div>
-      <div className="w-full max-w-4xl flex justify-center items-center overflow-hidden">
-        <Carousel current={current} />
-      </div>
-      <div className="flex justify-center mt-4">
-        <button
-          className="text-white bg-gray-800 p-2 rounded-full hover:bg-gray-700 mx-2"
-          onClick={prevSlide}
-        >
-          <FaChevronLeft size={24} />
-        </button>
-        <button
-          className="text-white bg-gray-800 p-2 rounded-full hover:bg-gray-700 mx-2"
-          onClick={nextSlide}
-        >
-          <FaChevronRight size={24} />
-        </button>
+      <div className="relative w-full max-w-4xl flex flex-col items-center px-4">
+        <h2 className="text-4xl font-bold mb-2 text-center w-full">Our Projects</h2>
+        <hr className="border-t-2 border-gray-600 w-3/4 mb-8 self-center" />
+        {/* <button
+            className="text-white bg-gray-800 p-2 rounded-full hover:bg-gray-700 mx-2"
+            onClick={prevSlide}
+          >
+            <FaChevronLeft size={24} />
+          </button>
+          <button
+            className="text-white bg-gray-800 p-2 rounded-full hover:bg-gray-700 mx-2"
+            onClick={nextSlide}
+          >
+            <FaChevronRight size={24} />
+          </button> */}
+        <div className="w-full max-w-4xl flex justify-center items-center overflow-hidden relative">
+          <Carousel current={current} />
+        </div>
       </div>
     </motion.main>
   );
